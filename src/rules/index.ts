c11ca8eab594c0f2111cd2b0c722947f8865c4a9
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-07-23 19:49:06 +0200

    build: lint files

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,8 +1,8 @@
 module.exports = {
-  rulesDirectory: '.',
   rules: {
     'ngrx-action-hygiene': true,
     'ngrx-no-duplicate-action-types': true,
     'ngrx-unique-reducer-actions': true,
   },
+  rulesDirectory: '.',
 }
