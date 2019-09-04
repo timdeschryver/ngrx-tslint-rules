701ead99f09f51786651982555699f9d049ae78f
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-09-04 17:43:43 +0200

    fix: add install step in schematic

--- src/schematics/ng-add/index.ts
+++ src/schematics/ng-add/index.ts
@@ -5,14 +5,23 @@ import {
   SchematicsException,
   Tree,
 } from '@angular-devkit/schematics'
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks'
 
 import { Schema } from './schema'
 
 export default function(options: Schema): Rule {
   return (host: Tree, context: SchematicContext) => {
     return chain([
+      updateDependencies(),
       extendTSLintRules(options.path || './tslint.json', options.rules),
     ])(host, context)
+  }
+}
+
+function updateDependencies(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    context.addTask(new NodePackageInstallTask())
+    return tree
   }
 }
 
