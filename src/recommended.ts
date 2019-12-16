56b3c3bccb1a288466e7d8a9a7ee96f9950ab3ab
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-12-16 12:24:17 +0100

    feat: add ngrx-no-dispatch-in-effects rule

--- src/recommended.ts
+++ src/recommended.ts
@@ -2,6 +2,7 @@ module.exports = {
   rules: {
     'ngrx-action-hygiene': true,
     'ngrx-effect-creator-and-decorator': true,
+    'ngrx-no-dispatch-in-effects': true,
     'ngrx-no-duplicate-action-types': true,
     'ngrx-no-typed-store': true,
     'ngrx-selector-for-select': true,
