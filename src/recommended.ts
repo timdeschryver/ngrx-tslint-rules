fdae0a73f8e6d42c9f284b00795c361e194c90c1
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-12-16 14:45:21 +0100

    fix: add severaty to recommended rules

--- src/recommended.ts
+++ src/recommended.ts
@@ -1,12 +1,26 @@
 module.exports = {
   rules: {
-    'ngrx-action-hygiene': 'warning',
-    'ngrx-avoid-dispatching-multiple-actions-sequentially': 'warning',
-    'ngrx-effect-creator-and-decorator': 'error',
-    'ngrx-no-dispatch-in-effects': 'error',
-    'ngrx-no-duplicate-action-types': 'error',
-    'ngrx-no-typed-store': 'error',
-    'ngrx-selector-for-select': 'error',
+    'ngrx-action-hygiene': {
+      severity: 'warning',
+    },
+    'ngrx-avoid-dispatching-multiple-actions-sequentially': {
+      severity: 'warning',
+    },
+    'ngrx-effect-creator-and-decorator': {
+      severity: 'error',
+    },
+    'ngrx-no-dispatch-in-effects': {
+      severity: 'warning',
+    },
+    'ngrx-no-duplicate-action-types': {
+      severity: 'error',
+    },
+    'ngrx-no-typed-store': {
+      severity: 'warning',
+    },
+    'ngrx-selector-for-select': {
+      severity: 'error',
+    },
   },
   rulesDirectory: './rules',
 }
